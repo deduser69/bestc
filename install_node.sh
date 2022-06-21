e6fee3aa770b1c4dbb6a0f6d078b8e6b37f35a5f
deduser69 <107910062+deduser69@users.noreply.github.com>
2022-06-21 15:15:05 +0530

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -1,10 +1,6 @@
-echo "deb https://deb.nodesource.com/node_16.x buster main" > /etc/apt/sources.list.d/nodesource.list && \
-  wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - && \
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list && \
-  wget -qO- https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
-  apt-get update && \
-  apt-get install -yqq nodejs yarn && \
-  pip install -U pip && pip install pipenv && \
-  npm i -g npm@^7 && \
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python && ln -s /root/.poetry/bin/poetry /usr/local/bin && \
-  rm -rf /var/lib/apt/lists/*
+command -v npm &>/dev/null && return
+	pprint "Installing Nodejs and Npm..  "
+	curl -fssL https://deb.nodesource.com/setup_17.x | sudo -E bash - &>nodelog.txt &&
+	sudo apt install nodejs -y &>>nodelog.txt &&
+	sudo npm i -g npm &>>nodelog.txt &&
+	pprint "SUCCESS!\n" "cgreen" || (pprint "FAIL.\n" "cred"; exit 1)
